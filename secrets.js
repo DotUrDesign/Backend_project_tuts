620a6a345935cd06d88fcdc374705e3c8fba9b36
Pratyush_Sahoo <91837580+DotUrDesign@users.noreply.github.com>
2023-12-05 10:00:57 +0530

    nothing special - just added some comments

--- secrets.js
+++ secrets.js
@@ -1,5 +1,5 @@
 const JWT_KEY = 'duchdaoifuc2382e273jhsbdcku';
 const DB_LINK = 'mongodb+srv://Prats:Prats123@cluster0.iy4gzd4.mongodb.net/?retryWrites=true&w=majority';
-const SK = 'sk_test_51OHQFkSHzdlyCKbYcDg76hDlT44H58YnTFnRmiS1UR5R1N7PyokBO4QfDy5x7PNaQy8A0OOHj6wiGq68dqQVwUwF00XG4ZLVh2';
+const SK = 'sk_test_51OHQFkSHzdlyCKbYcDg76hDlT44H58YnTFnRmiS1UR5R1N7PyokBO4QfDy5x7PNaQy8A0OOHj6wiGq68dqQVwUwF00XG4ZLVh2'; // stripe => checkout
 
 module.exports = {JWT_KEY,DB_LINK,SK};
